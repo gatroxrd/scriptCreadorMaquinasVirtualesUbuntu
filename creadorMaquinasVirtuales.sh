60d00103623538f816b2f25bb313459537f31179
Gabriel Rodríguez Domínguez <52460428+gatroxrd@users.noreply.github.com>
2024-03-25 19:08:04 -0600

    Update creadorMaquinasVirtuales.sh

--- creadorMaquinasVirtuales.sh
+++ creadorMaquinasVirtuales.sh
@@ -10,6 +10,25 @@
 export NombreMunicipio=""
 export idInegi=0
 
+softwareRequerido()
+{
+	#Docker ----------------------------------------------------------------------------------------------------------------------
+ 	sudo apt update
+  	sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+   	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    	echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" | sudo tee /etc/apt/sources.list.d/docker.list
+
+     	sudo apt update
+        sudo apt install docker-ce docker-ce-cli containerd.io
+	#-------------1----------------------------------------------------------------------------------------------------------------
+
+	# NGINX -----------------------------------------------------
+	sudo apt  install nginx
+ 	sudo systemctl enable nginx
+ 	#------------------------------------------------------------
+    
+}
+
 copiadoCarpetasFuente()
 {
 	clear
